203b4e7daab8be605e98ea1b2fc5ad8e76ca4ce7
Wayne Araza <aerionne.araza@olivarezcollegetagaytay.edu.ph>
2025-11-19 21:42:36 +0800

    Revert to Erika Soundtrack Ver

--- My2DGame/src/main/AssetSetter.java
+++ My2DGame/src/main/AssetSetter.java
@@ -1,6 +1,8 @@
 package main;
 
-import entity.*;
+import entity.NPC_OldMan;
+import entity.NPC_Slime;
+import entity.NPC_Slime2;
 import object.OBJ_Boots;
 import object.OBJ_Chest;
 import object.OBJ_Door;
@@ -21,7 +23,7 @@ public class AssetSetter {
 
     public void setNPC(){
         gp.npc[0] = new NPC_OldMan(gp);
-        gp.npc[0].worldX = gp.tileSize*20;
+        gp.npc[0].worldX = gp.tileSize*21;
         gp.npc[0].worldY = gp.tileSize*21;
 
         gp.npc[1] = new NPC_Slime(gp);
@@ -31,18 +33,6 @@ public class AssetSetter {
         gp.npc[2] = new NPC_Slime2(gp);
         gp.npc[2].worldX = gp.tileSize*31;
         gp.npc[2].worldY = gp.tileSize*27;
-
-        gp.npc[3] = new Sign(gp);
-        gp.npc[3].worldX = gp.tileSize*21;
-        gp.npc[3].worldY = gp.tileSize*19;
-
-        gp.npc[4] = new Sign2(gp);
-        gp.npc[4].worldX = gp.tileSize*25;
-        gp.npc[4].worldY = gp.tileSize*19;
-
-        gp.npc[5] = new Door(gp);
-        gp.npc[5].worldX = gp.tileSize*27;
-        gp.npc[5].worldY = gp.tileSize*18;
 
     }
 }
