692fcb5f4e4ef7adb5902f63c316dd5ddda3ff42
alponseo <alfonso.caralipio@olivarezcollegetagaytay.edu.ph>
2025-11-19 14:24:13 +0800

    sign and door interactable

--- My2DGame/src/main/AssetSetter.java
+++ My2DGame/src/main/AssetSetter.java
@@ -1,8 +1,6 @@
 package main;
 
-import entity.NPC_OldMan;
-import entity.NPC_Slime;
-import entity.NPC_Slime2;
+import entity.*;
 import object.OBJ_Boots;
 import object.OBJ_Chest;
 import object.OBJ_Door;
@@ -23,7 +21,7 @@ public class AssetSetter {
 
     public void setNPC(){
         gp.npc[0] = new NPC_OldMan(gp);
-        gp.npc[0].worldX = gp.tileSize*21;
+        gp.npc[0].worldX = gp.tileSize*20;
         gp.npc[0].worldY = gp.tileSize*21;
 
         gp.npc[1] = new NPC_Slime(gp);
@@ -33,6 +31,18 @@ public class AssetSetter {
         gp.npc[2] = new NPC_Slime2(gp);
         gp.npc[2].worldX = gp.tileSize*31;
         gp.npc[2].worldY = gp.tileSize*27;
+
+        gp.npc[3] = new Sign(gp);
+        gp.npc[3].worldX = gp.tileSize*21;
+        gp.npc[3].worldY = gp.tileSize*19;
+
+        gp.npc[4] = new Sign2(gp);
+        gp.npc[4].worldX = gp.tileSize*25;
+        gp.npc[4].worldY = gp.tileSize*19;
+
+        gp.npc[5] = new Door(gp);
+        gp.npc[5].worldX = gp.tileSize*27;
+        gp.npc[5].worldY = gp.tileSize*18;
 
     }
 }
